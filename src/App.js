aab1a45d559454694d752faa17cf6c4c8964d556
GoguElena <158441870+GoguElena@users.noreply.github.com>
2024-03-14 20:29:31 +0200

    only reactjs directory

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          CR-222 Gogu Elena
+        </p>
+        <a
+          className="App-link"
+          href="https://else.fcim.utm.md/course/view.php?id=634"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          TWeb ELSE
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
